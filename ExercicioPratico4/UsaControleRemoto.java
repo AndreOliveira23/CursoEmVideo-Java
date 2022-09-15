f7bfcceacdc63603b8c70bd6ce0cb545569bdaa2
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2022-09-14 21:35:15 -0300

    Add 'UsaControleRemoto.java'
    Arquivo de exemplo de uso dos métodos implementados em 'ControleRemoto.java'

---
+++ ExercicioPratico4/UsaControleRemoto.java
@@ -0,0 +1,11 @@
+public class UsaControleRemoto {
+	public static void main(String[] args){
+		
+		ControleRemoto c1 = new ControleRemoto();
+		c1.ligar();
+		c1.aumentarVolume();
+		c1.play();
+		c1.abrirMenu();
+		c1.fecharMenu();	
+	}
+}
